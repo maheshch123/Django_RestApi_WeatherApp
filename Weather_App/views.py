7e9c1fd985ddcbad3d4c914b4c85b0f7b4f65caf
mahesh <Maheshchennampalli1995@gmail.com>
2021-01-03 13:28:48 +0530

    Update views.py

--- Weather_App/views.py
+++ Weather_App/views.py
@@ -4,7 +4,7 @@ from pprint import pprint
 
 # Create your views here.
 def weather(request):
-    url = "http://127.0.0.1:8000/RestApiRestApi/?search={}&"
+    url = "https://maheshch123-django-restapi-weatherapp-8624.zeet.app//RestApiRestApi/?search={}&"
     headers = {'Authorization': 'Token 5d27228a4a22bb7125dcf2b5fe2df020c424c040'}
     city = request.GET.get('city')
     r = requests.get(url.format(city),headers=headers).json()
