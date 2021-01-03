3a868298f55bcdf4cabc9758675b4de97414f371
mahesh <Maheshchennampalli1995@gmail.com>
2021-01-03 12:31:36 +0530

    Update urls.py

--- RestApiKey_App/urls.py
+++ RestApiKey_App/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('Home',views.base, name='base'),
+    path('',views.base, name='base'),
     path('register',views.register, name='register'),
     path('login',views.login, name='login'),
     path('logout',views.logout, name='logout'),
